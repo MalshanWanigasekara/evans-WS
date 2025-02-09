b8e7700f63d87781b411fb556fe7399b70011d22
malshanucsc <randhajapvt@gmail.com>
2025-02-09 23:01:16 +0530

    project initial structure added

---
+++ src/main/java/com/example/evansWS/EvansWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.evansWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvansWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvansWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evansWS/EvansWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evansWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvansWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
